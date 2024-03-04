9e01f8fe9f7620eaa33c52d56ece0adb9e91452b
Vyacheslav-Belov <vyacheslav.belov@cgtrader.com>
2024-03-04 16:19:54 +0200

    Add python and cpp script files

---
+++ cpp/findCollision/findCollision.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+
+#include <maya/MString.h>
+#include <maya/MArgList.h>
+
+#include <maya/MFnPlugin.h>
+#include <maya/MGlobal.h>
+#include <maya/MItSelectionList.h>
+#include <maya/MPxCommand.h>
+#include <maya/MFnMesh.h>
+#include <maya/MDagPath.h>
+#include <maya/MVector.h>
+#include <maya/MSelectionList.h>
+#include <maya/MMatrix.h>
+
+#include <maya/miostream.h>
+
+class findCollision : public MPxCommand
+{
+    public:
+        MStatus doIt( const MArgList& args );
+        static void* creator();
+};
+
+void* findCollision::creator() {
+    return new findCollision;
+}
+
+MStatus initializePlugin( MObject obj ) {
+    MFnPlugin plugin( obj, "Viacheslav", "1.0", "Any" );
+    plugin.registerCommand( "FindCollision", findCollision::creator );
+    return MS::kSuccess;
+}
+
+MStatus uninitializePlugin( MObject obj ) {
+    MFnPlugin plugin( obj );
+    plugin.deregisterCommand( "FindCollision" );
+    return MS::kSuccess;
+}
+
+MStatus findCollision::doIt( const MArgList& args ) {
+    MStatus status = MS::kSuccess;
+    
+    MSelectionList selection_list;
+    MGlobal::getActiveSelectionList(selection_list);
+    
+    MDagPath mesh_dag;
+    selection_list.getDagPath(0, mesh_dag);
+    MDagPath locator_dag;
+    selection_list.getDagPath(1, locator_dag);
+    MFnMesh mesh(mesh_dag, &status);
+
+    MMatrix locator_matrix;
+    locator_matrix = locator_dag.inclusiveMatrix();
+    MTransformationMatrix m_matrix(locator_matrix);
+
+    MVector ray_source;
+    ray_source = m_matrix.translation(MSpace::kWorld);
+    MFloatPoint ray_source_float_point(ray_source);
+    
+    MFloatPoint hit_point;
+    MMeshIsectAccelParams accel_params = mesh.autoUniformGridParams();
+    float hit_ray_param, hit_bary_1, hit_bary_2;
+    int hit_face, hit_triangle;
+
+    float ray_directions[3][3] = {
+        {1.0, 0.0, 0.0},
+        {0.0, 1.0, 0.0},
+        {1.0, 0.0, 1.0}
+    };
+    bool intersects;
+    for (int i=0; i < 3; i++) {
+        intersects = mesh.closestIntersection(
+            ray_source,
+            ray_directions[i],
+            NULL,
+            NULL,
+            false,
+            MSpace::kWorld,
+            9999,
+            false,
+            &accel_params,
+            hit_point,
+            &hit_ray_param,
+            &hit_face,
+            &hit_triangle,
+            &hit_bary_1,
+            &hit_bary_2,
+            (float)1e-6,
+            &status
+        );
+        if (intersects) {
+            cout << hit_point << endl;
+            return status;
+        };
+    };
+
+    return status;
+}
---
+++ python/find-closest-collision-point.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+import maya.api.OpenMaya as om
+import pymel.core as pm
+from typing import Optional
+
+
+def find_closest_collision_point() -> om.MFloatPoint | None:
+    """
+    1. Select mesh.
+    2. Select locator to check for collisions with mesh.
+    3. Execute this function.
+    
+    Returns:
+        (X, Y, Z) coordinates of the collision point, if found.
+        None otherwise.
+    """
+    selection_list = om.MGlobal.getActiveSelectionList()
+
+    mesh = om.MFnMesh(selection_list.getDagPath(0))
+    locator_dag = selection_list.getDagPath(1)
+    
+    m_matrix = om.MTransformationMatrix(locator_dag.inclusiveMatrix())
+    ray_source = m_matrix.translation(om.MSpace.kWorld)
+    ray_source_float_point = om.MFloatPoint(*ray_source)
+
+    ray_directions = (
+        (1.0, 0.0, 0.0),
+        (0.0, 1.0, 0.0),
+        (0.0, 0.0, 1.0),
+    )
+    intersections = []
+    for ray_direction in ray_directions:
+        intersection = mesh.anyIntersection(
+            ray_source_float_point,  # raySource
+            om.MFloatVector(*ray_direction),  #rayDirection
+            om.MSpace.kWorld,  # space
+            10000,  # maxParam
+            False,  # testBothDirections
+            idsSorted=False,
+            accelParams=mesh.autoUniformGridParams(),
+            tolerance=0.0001,
+        )
+        if intersection is not None:
+            hit_point, hit_ray_param, hit_face, hit_triangle, hit_bary_1, hit_bary_2 = intersection
+            intersections.append(hit_point)
+
+    if intersections:
+        return min(intersections, key=lambda hp: hp.distanceTo(ray_source_float_point))
+
+
+def create_locator(m_float_point: om.MFloatPoint) -> None:
+    """
+    Creates a new locator at the "m_float_point" coordinates.
+    """
+    loc = pm.spaceLocator()
+    loc.setTranslation(
+        (m_float_point.x, m_float_point.y, m_float_point.z),
+        ws=True,
+    )
+
+
+closest_collision_point = find_closest_collision_point()
+if closest_collision_point:
+    create_locator(closest_collision_point)
